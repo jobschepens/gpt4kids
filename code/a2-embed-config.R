d0bceaa24842f9d4e4201332cd33ac42ef9da426
Job Schepens <job.schepens@uni-koeln.de>
2025-07-21 14:39:11 +0200

    bin graphs

--- code/a2-embed-config.R
+++ code/a2-embed-config.R
@@ -5,7 +5,7 @@ FIGURE_WIDTH <- 8
 FIGURE_HEIGHT <- 7
 
 # File paths
-FASTTEXT_FILE <- "../data-original/cc.de.300.bin" # or ../data-original/cc.de.300.vec
+FASTTEXT_FILE <- "../data-original/cc.de.300.bin/cc.de.300.bin" # or ../data-original/cc.de.300.vec
 DATA_FILE <- "../data-processed/all_wider_again_subt_ch_plus1_prob.RData"
 UDPIPE_MODEL <- "../data-original/german-hdt-ud-2.5-191206.udpipe"
 CACHE_FILE <- "../data-processed/translation_cache.RData"
